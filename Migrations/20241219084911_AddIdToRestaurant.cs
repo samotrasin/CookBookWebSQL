d06fc9d54ef6f7200b3af3e8124ea656758cb2c1
ngetponleu <punluenget282004@gmail.com>
2024-12-19 15:53:16 +0700

    updated

---
+++ Migrations/20241219084911_AddIdToRestaurant.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookBookWebSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdToRestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
